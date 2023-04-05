3b2a8cb2ba71e2cc191d587bd58b5d095e1d62ca
Jazmin132 <jazi2000.ja@gmail.com>
2023-04-04 21:34:14 -0300

    Cámara Cinemachine
    Co-Authored-By: Vicky33o1 <128933366+Vicky33o1@users.noreply.github.com>

--- Assets/Scenes/Scripts/Player/Camara.cs
+++ Assets/Scenes/Scripts/Player/Camara.cs
@@ -15,21 +15,4 @@ public class Camara : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-    private void Update()
-    {
-        float H = Input.GetAxis("Mouse X") * _sensitivity;
-        float V = Input.GetAxis("Mouse Y") * _sensitivity;
-
-        _currentX += H;
-        _currentY -= V;
-        _currentY = Mathf.Clamp(_currentY, -5f, 33f);
-
-        Quaternion rotation = Quaternion.Euler(_currentY, _currentX, 0);
-
-        Vector3 position = rotation * new Vector3(0f, 0f, -_distance) + _Player.position;
-
-        transform.LookAt(_Player.position);
-        transform.rotation = rotation;
-        transform.position = position;
-    }
 }
