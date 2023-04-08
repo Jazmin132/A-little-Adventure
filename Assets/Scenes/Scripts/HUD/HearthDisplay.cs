abe7db154335ba17b2f3697206210619784afe10
JuliusRumanus <julian.losiscky@davinci.edu.ar>
2023-04-08 20:08:56 -0300

    HudImplemented
    Le puse un sprite de corazones y un script en la carpeta de scripts HUD

---
+++ Assets/Scenes/Scripts/HUD/HearthDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HearthDisplay : MonoBehaviour
+{
+    public Image[] hearthIcons;
+    
+    private void Awake()
+    {
+        hearthIcons = GetComponentsInChildren<Image>();
+    }
+
+    public void UpdateHealth(int currentHealth)
+    {
+        for (int i = 0; i < hearthIcons.Length; i++)
+        {
+            hearthIcons[i].enabled = (currentHealth > i);
+        }
+    }
+}
