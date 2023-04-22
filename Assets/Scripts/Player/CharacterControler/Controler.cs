c63d37b285aa7a0e013d2d7e67d658b6e0855bb4
49216 <130787808+49216@users.noreply.github.com>
2023-04-22 19:28:24 -0300

    A-little-adventure-1.12
    Cambios del Proyecto:
    
    - Arreglo del control del jugador.
    - Agregue los "ingredientes" al nivel.
    - Agregue también la plataforma debajo del nivel.
    - Agrego las tuercas del nivel.

--- Assets/Scripts/Player/CharacterControler/Controler.cs
+++ Assets/Scripts/Player/CharacterControler/Controler.cs
@@ -22,16 +22,17 @@ public class Controler : IController
         _M.MovePlayer(H, V);
         //No permite mas de dos teclas al mismo tiempo??????
         //Mas que nada, no permite saktar al moverse (al correr)
-        if (Input.GetKeyDown(KeyCode.Space))
-            _M.Jump();
-        else if (Input.GetKey(KeyCode.Z))
+        if (Input.GetKey(KeyCode.Space)) _M.Jump();
+        
+        if (Input.GetKey(KeyCode.Z))
             _M.Glide();
-
-        if (Input.GetKeyDown(KeyCode.X))
+        
+        else if (Input.GetKey(KeyCode.X))
             _M.Attack();
-        else if(Input.GetKeyUp(KeyCode.C))
+        
+        else if(Input.GetKey(KeyCode.C))
             _M.Shoot();
-
+        
         if (Input.GetKey(KeyCode.LeftShift))
             _M.Run();
         else if(Input.GetKeyUp(KeyCode.LeftShift)) _M.RunReset();
