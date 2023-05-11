0796e821c32540f28de54453ebe3c0174a304ab8
Jazmin132 <jazi2000.ja@gmail.com>
2023-05-11 16:25:13 -0300

    Ojala funcione

--- Assets/2_Scripts/Game/Player/CharacterControler/Controler.cs
+++ Assets/2_Scripts/Game/Player/CharacterControler/Controler.cs
@@ -32,27 +32,18 @@ public class Controler : IController
     {
         return Input.GetKey(KeyCode.Z);
     }
-    public void ListenKey()
-    {
-        var V = Input.GetAxis("Vertical");
-        var H = Input.GetAxis("Horizontal");
-
-        _M.MovePlayer(H, V);
-        //No permite mas de dos teclas al mismo tiempo, problema de windows
-        if (Input.GetKeyDown(KeyCode.Space)) _M.Jump();
-        else if (Input.GetKey(KeyCode.Z)) _M.Glide();
-
-        if (Input.GetKey(KeyCode.E))
-            _M.Attack();
-        else if(Input.GetKeyDown(KeyCode.C))
-            _M.Shoot();
-        
-        if (Input.GetKey(KeyCode.LeftShift))
-            _M.Run();
-        else if(Input.GetKeyUp(KeyCode.LeftShift)) _M.RunReset();
-    }
     public bool Acelerate()
     {
         return (Input.GetKey(KeyCode.LeftShift));
     }//else if(Input.GetKeyUp(KeyCode.LeftShift)) _M.RunReset();
+
+    public bool Attack()
+    {
+        return (Input.GetKey(KeyCode.E));
+    }
+
+    public bool Shoot()
+    {
+        return (Input.GetKey(KeyCode.Mouse1));
+    }
 }
