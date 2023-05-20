6d32e561f2f9ae92dd956f0a63111e7180de6494
Jazmin132 <jazi2000.ja@gmail.com>
2023-05-20 17:34:34 -0300

    Something

--- Assets/2_Scripts/Game/PlayerRelated/CharacterControler/FSM-States/AirState.cs
+++ Assets/2_Scripts/Game/PlayerRelated/CharacterControler/FSM-States/AirState.cs
@@ -98,6 +98,8 @@ public class AirState : IState
         _Right = Vector3.ProjectOnPlane(_MainCamera.transform.right, Vector3.up).normalized;
         _direction = (_Controller.Horizontal() * _Right + _Controller.Vertical() * _Forward).normalized;
 
+        if (_direction.sqrMagnitude > 1) _direction.Normalize();
+
         if (_Player.WallDetecter(_direction)) return;
 
         if (_Controller.Horizontal() != 0 || _Controller.Vertical() != 0)
