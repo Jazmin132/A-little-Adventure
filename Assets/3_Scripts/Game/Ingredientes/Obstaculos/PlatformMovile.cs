88a08875e2fd4a1ccdbd409d2e9cc767f6085c5e
Jazmin132 <jazi2000.ja@gmail.com>
2023-06-15 17:11:54 -0300

    Minor changes

--- Assets/3_Scripts/Game/Ingredientes/Obstaculos/PlatformMovile.cs
+++ Assets/3_Scripts/Game/Ingredientes/Obstaculos/PlatformMovile.cs
@@ -5,7 +5,7 @@ public class PlatformMovile : MonoBehaviour
     [SerializeField] Transform[] _WayPoint;
     [SerializeField] GameObject _Platform;
     [SerializeField] float _Speed;
-    [SerializeField] int _count;
+    int _count = 0;
     Vector3 _Dir;
 
     private void Start()
@@ -20,7 +20,7 @@ public class PlatformMovile : MonoBehaviour
         if (Vector3.Distance(_Platform.transform.position, _WayPoint[_count].position) < 1f)
         {
             _count++;
-            //ej _count 0-4  _WayPoint.Length 5 (empieza desde 1)
+            //_WayPoint.Length (empieza a contar desde 1)
             if (_count >= _WayPoint.Length) 
                 _count = 0;
         }
