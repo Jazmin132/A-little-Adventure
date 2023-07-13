f82b9935bc6e7e87b09824350fb1d5c37609972b
49216 <130787808+49216@users.noreply.github.com>
2023-07-13 05:39:56 -0300

    A-little-Adventure-2.39
    Cambios:
    
    - Acabo de hacer 3 piezas vitales los cuales son 3 piezas del avión (Rueda, hélice y Alas) que se pueden encontrar en la carpeta de Prefabs en donde estaba el Barril.

--- Assets/3_Scripts/Game/Ingredientes/PowerUps/JumpPower.cs
+++ Assets/3_Scripts/Game/Ingredientes/PowerUps/JumpPower.cs
@@ -4,19 +4,25 @@ using System.Collections;
 public class JumpPower : PowerUp
 {
     [SerializeField] float time; 
-    [SerializeField] float JumpForceAdded; 
+    [SerializeField] float JumpForceAdded;
+    public BoxCollider Box;
 
-
+    public void Awake()
+    {
+        Box = GetComponent<BoxCollider>();
+    }
     public override void Activate(PlayerM Player)
     {
         Player.DoubleJump(time, JumpForceAdded);
         Debug.Log("Activate double Jump");
-        this.gameObject.SetActive(false);
         StartCoroutine(ForHowLongGone(5));
     }
     IEnumerator ForHowLongGone(float time)
     {
+        Box.enabled = false;
+        gameObject.SetActive(false);
         yield return new WaitForSeconds(time);
-        this.gameObject.SetActive(true);
+        gameObject.SetActive(true);
+        Box.enabled = true;
     }
 }
