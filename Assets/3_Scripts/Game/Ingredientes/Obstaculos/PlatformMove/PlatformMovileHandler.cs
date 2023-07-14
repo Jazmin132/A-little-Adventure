1a7f8eb31607ab146524469a33130df73055fde9
Jazmin132 <jazi2000.ja@gmail.com>
2023-07-14 18:22:38 -0300

    Plataforma

--- Assets/3_Scripts/Game/Ingredientes/Obstaculos/PlatformMove/PlatformMovileHandler.cs
+++ Assets/3_Scripts/Game/Ingredientes/Obstaculos/PlatformMove/PlatformMovileHandler.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlatformMovileHandler : MonoBehaviour
 {
     [SerializeField] Transform[] _WayPoint;
-    [SerializeField] GameObject _Platform;
+    [SerializeField] Transform _Platform;
     [SerializeField] float _Speed;
     int _count = 0;
     Vector3 _Dir;
@@ -15,9 +15,9 @@ public class PlatformMovileHandler : MonoBehaviour
     public void FixedUpdate()
     {
         _Dir = _WayPoint[_count].position - _Platform.transform.position;
-        _Platform.transform.position += _Dir.normalized * _Speed * Time.fixedDeltaTime;
+        _Platform.position += _Dir.normalized * _Speed * Time.fixedDeltaTime;
 
-        if (Vector3.Distance(_Platform.transform.position, _WayPoint[_count].position) < 1f)
+        if (Vector3.Distance(_Platform.position, _WayPoint[_count].position) < 1f)
         {
             _count++;
             //_WayPoint.Length (empieza a contar desde 1)
